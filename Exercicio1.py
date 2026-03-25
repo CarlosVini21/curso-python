2c6d125486b983cc3b5515e60eecb858d69b5ab4
CarlosVini21 <carlosviniciusmiranda012@hotmail.com>
2026-03-25 20:02:14 -0300

    Add files via upload

---
+++ Exercicio1.py
@@ -0,0 +1,23 @@
+print("Digite seu nome")
+nome =input()
+
+print("qual e o seu sobrenome")
+sobrenome=input()
+
+print("Data de Nascimento")
+nasc=input()
+
+print("genero")
+gen=input()
+
+print("numero de celular")
+celular=input()
+
+print("senha")
+senha=input()
+print("Digite seu nome")
+input()
+print("digite seu sobrenome")
+input()
+print(nome+sobrenome)
+

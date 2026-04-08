84a76e60712be9502735476ebcff7f97396d29e9
CarlosVini21 <carlosviniciusmiranda012@hotmail.com>
2026-04-08 19:34:59 -0300

    Create exercicio6.py

---
+++ exercicio6.py
@@ -0,0 +1,13 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+#ler um valor e dizer se é positivo ou negativo
+print("Try programiz.pro")
+
+print("digite um numero")
+numero=float(input())
+
+if numero>0:
+        print("o numero positivo!")
+else:
+    print(" o negativo!")

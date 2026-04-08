9c0f7d8dbfd10de37babd61464df2efb133af761
CarlosVini21 <carlosviniciusmiranda012@hotmail.com>
2026-04-08 19:32:17 -0300

    Create exercicio 6.py

---
+++ exercicio 6.py
@@ -0,0 +1,13 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+#faça um algoritmo que peça um numero e informe se ele e maior que 10
+print("Try programiz.pro")
+
+print("digite um numero")
+numero=int(input())
+
+if numero>10:
+        print("o numero é maior que 10!")
+else:
+    print(" o numero é menor que 10!")

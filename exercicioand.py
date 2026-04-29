d77e575788638a0d84d3b0bbe7e0ee8248aa2498
CarlosVini21 <carlosviniciusmiranda012@hotmail.com>
2026-04-29 19:26:16 -0300

    Create exercicioand.py

---
+++ exercicioand.py
@@ -0,0 +1,18 @@
+print("Try programiz.pro")
+print("digite o primeiro numero")
+numero1=float(input())
+print("digite o segundo numero")
+numero2=float(input())
+print("digite o terceiro numero")
+numero3=float(input())
+
+if numero1>numero2:
+    print("numero dois é maior")
+else:
+    if numero1>numero3:
+        print("o numero 1 é o maior numero",numero1)
+    print("o terceiro é o maior",numero3)
+if numero2>numero3:
+    print("o numero 2 é o maior",numero2)
+    
+    

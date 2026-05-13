2c7ef12666227c2d6d70298bbe4bfa98c8804266
CarlosVini21 <carlosviniciusmiranda012@hotmail.com>
2026-05-13 18:48:17 -0300

    Update exercicofor2.py

--- exercicofor2.py
+++ exercicofor2.py
@@ -8,23 +8,36 @@ soma=0
 
 for num in numero:
     if (num %2)==0:
-        pares = pares + 1
+        pares = pares+ 1
         
     else:
-        impar=impar +1
+        impar= impar+1
         
+soma = soma + num
+media = soma/10
+
+numero=[10,7,5,6,8,4,3,2,1,0]
+pares=0
+impar=0
+maior=0
+menor=0
+media=0
+soma=0
+
 for num in numero:
+    if (num % 2)==0:
+        pares = pares + 1
+    else:
+        impar=impar +1
+    if num > maior:
+        maior=num
+    else:
+        num < menor
+        menor = num
+        
     soma = soma + num
 media = soma/10
 
-
-    
-for num in numero:
-    if num > maior:
-        maior=num
-for num in numero:
-    if num < menor:
-     menor = num
      
 print("esse numero é par",pares)
 print("esse numero é impar",impar)

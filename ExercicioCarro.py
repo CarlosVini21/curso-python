f9646d1ff5d1e3af40bfdb8edd745afaca4d7feb
CarlosVini21 <carlosviniciusmiranda012@hotmail.com>
2026-05-13 20:17:12 -0300

    Create ExercicioCarro.py

---
+++ ExercicioCarro.py
@@ -0,0 +1,10 @@
+varValorCarro=0
+
+print("Digite o valor do Carro")
+varValorCarro=float (input())
+
+valorCalculo = varValorCarro *(0.28 + 0.45) 
+
+valorFinal = valorCalculo + varValorCarro
+
+print("o valor final é ",valorFinal)
